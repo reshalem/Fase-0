ed561a6e00f207487c334ace636cf7c1f24c1583
reshalem <aliisetia@gmail.com>
2018-07-31 12:29:35 +0700

    Tugas Hari Senin WEEK 2

---
+++ simple_psudocode.js
@@ -0,0 +1,47 @@
+// 1. NEWTON SECOND LAW
+
+// STORE "mass" with value 600 (kg)
+// STORE "acceleration" with value 2 (m/s2)
+// STORE "force" with no value
+
+// CALCULATE "mass" times "acceleration"
+// SET "force" value with calculation result
+
+// DISPLAY "force"
+
+
+
+// 2. TAHUN KABISAT
+
+// STORE "tahun" with any number value
+
+// IF (("tahun" mod 4 == 0 AND "tahun" mod 100 != 0)) OR ("tahun" mod 400 == 0)
+//   DISPLAY "Tahun " + tahun + " adalah tahun kabisat."
+// ELSE 
+//   DISPLAY "Tahun " + tahun + " bukan merupakan tahun kabisat."
+
+
+
+// 3. LAUNDRY DAY
+
+// STORE "count" with value 0
+
+// FOR (var "i" = 0; "i" < 20; "i"++) 
+//   CALCULATE "count" = "count" + 1
+
+// IF "count" = 20 
+//   DISPLAY "Semua pakaian sudah dimasukkan ke mesin cuci. Tombol on mesin cuci sudah dapat ditekan."
+// ELSE 
+//   DISPLAY "Belum semua pakaian dimasukkan. Silahkan hitung ulang lagi."
+
+
+
+// 4. PERIKSA KUKU
+
+// STORE "kuku_panjang" with any boolean value 
+
+// FOR (var "i" = 0; "i" < 40; "i"++)
+//   IF "kuku_panjang" === true
+//     DO hukum siswa
+//   ELSE 
+//     DO puji siswa
