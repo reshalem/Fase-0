56ec3164787886ab945fddf0748c1cc0baf6ce7d
reshalem <aliisetia@gmail.com>
2018-07-31 14:56:04 +0700

    Tugas Hari Selasa (2) WEEK 2

---
+++ Proxytia.js
@@ -0,0 +1,21 @@
+var nama = 'Jon';
+var peran = 'Ksatria';
+
+if (nama.length !== 0) {
+	if (peran.length !== 0) {
+		console.log('Selamat datang di Dunia Proxytia, ' + nama);
+
+		if (peran === 'Ksatria') {
+			console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+		} else if (peran === 'Tabib') {
+			console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.');
+		} else if (peran === 'Penyihir') {
+			console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+		}
+	} else {
+		console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
+	}
+} else {
+	console.log('Nama harus diisi!');
+}
+
